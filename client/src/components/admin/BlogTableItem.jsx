02626f660e94843b1b238ffb405773a59697f2b6
MrArnabGhosh <ghosharnab460@gmail.com>
2025-07-14 20:42:28 +0530

    create dashboard page

---
+++ client/src/components/admin/BlogTableItem.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { assets } from '../../assets/assets'
+
+const BlogTableItem = ({blog,fetchBlogs,index}) => {
+
+    const {title,createdAt}=blog
+    const blogDate = new Date(createdAt)
+
+  return (
+    <tr className='border-y border-gray-300'>
+        <th className='px-2 py-4'> {index} </th>
+        <td className='px-2 py-4'>{title}</td>
+        <td className='px-2 py-4 max-md:hidden'>{blogDate.toDateString()}</td>
+        <td className='px-2 py-4 max-md:hidden'>
+            <p className={`${blog.isPublished ? "text-green-600" : "text-orange-700"}`}>{blog.isPublished ? "Published" : "Unpublished"}</p>
+        </td>
+        <td className='px-2 py-4 text-xs gap-3 flex '>
+            <button className='rounded mt-1 px-2 py-0.5 border cursor-pointer'> 
+               {blog.isPublished ? " Unpublish" : "Publish"} 
+            </button>
+            <img src={assets.cross_icon} className='w-8 hover:scale-110 transition-all cursor-pointer' alt="" />
+        </td>
+    </tr>
+  )
+}
+
+export default BlogTableItem
