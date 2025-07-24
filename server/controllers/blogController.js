d3430f61b40fcdd1cc3ec85d133a5117c97fc0b1
MrArnabGhosh <ghosharnab460@gmail.com>
2025-07-24 20:49:08 +0530

    enhance blog management with CRUD operations and image upload

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,42 +1,97 @@
-import fs from 'fs'
-import imagekit from '../db/imagekit.js'
-import Blog from '../models/BlogModels.js'
+import fs from "fs";
+import imagekit from "../db/imagekit.js";
+import Blog from "../models/BlogModels.js";
 
+export const addBlog = async (req, res) => {
+  try {
+    const { title, subTitle, description, category, isPublished } = JSON.parse(
+      req.body.blog
+    );
 
-export const addBlog = async(req,res)=>{
+    const imageFile = req.file;
+
+    if (!title || !description || !category || !imageFile) {
+      return res.json({ success: false, message: "Missing required field" });
+    }
+
+    const fileBuffer = fs.readFileSync(imageFile.path);
+    //upload image to imageKit
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/blogs",
+    });
+
+    //optimized image through transformation
+    const optimizedImageUrl = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        { quality: "auto" },
+        { format: "webp" },
+        { width: "1280" },
+      ],
+    });
+
+    const image = optimizedImageUrl;
+
+    await Blog.create({
+      title,
+      subTitle,
+      description,
+      category,
+      image,
+      isPublished,
+    });
+
+    res.json({ success: true, message: "Blog added Successfully" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find({ isPublished: true });
+
+    res.json({ success: true, blogs });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getBlogById = async (req, res) => {
+  try {
+    const { blogId } = req.params;
+    const blog = await Blog.findById(blogId);
+
+    if (!blog) {
+      return res.json({ success: false, message: "Blog not found" });
+    }
+    res.json({ success: true, blog });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const deleteBlogById = async (req, res) => {
+  try {
+    const { id } = req.body;
+    await Blog.findByIdAndDelete(id)
+    res.json({ success: true, message:"Blog deleted Succesfully" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const togglePublish = async(req,res)=>{
     try {
-        const {title,subTitle,description ,category ,isPublished} = JSON.parse(req.body.blog)
+        const {id}  = req.body
+        const blog = await Blog.findById(id)
 
-        const imageFile = req.file
-
-        if(!title|| !description|| !category ||!imageFile){
-            return res.json({success:false , message:"Missing required field"})
-        }
-
-        const fileBuffer = fs.readFileSync(imageFile.path)
-        //upload image to imageKit
-        const response = await imagekit.upload({
-            file:fileBuffer,
-            fileName:imageFile.originalname,
-            folder:"/blogs"
-        })
-
-        //optimized image through transformation
-        const optimizedImageUrl = imagekit.url({
-            path:response.filePath,
-            transformation:[
-                {quality:'auto'},
-                {format:'webp'},
-                {width:'1280'}
-            ]
-        })
-
-        const image = optimizedImageUrl
-
-        await  Blog.create({title,subTitle,description ,category ,image,isPublished})
-
-        res.json({success:true , message:"Blog added Successfully"})
+        blog.isPublished = !blog.isPublished
+        await blog.save()
+        res.json({ success: true, message:"Blog status updated" });
     } catch (error) {
-       res.json({success:false , message:error.message}) 
+        res.json({ success: false, message: error.message });
     }
 }
